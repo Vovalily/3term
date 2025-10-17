f8575eec960ff4ba0fb9463e950e2eadc0dde893
Vovalily <toll.04@mail.ru>
2025-10-17 16:12:12 +0700

    create DataAccessLayer

---
+++ DataAccessLayer/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> where T : IDomainObject, new()
+    {
+        void Create(T obj);
+        IEnumerable<T> ReadAll();
+        T ReadById(int id);
+        void Update(T obj);
+        void Delete(T obj);
+
+    }
+}
